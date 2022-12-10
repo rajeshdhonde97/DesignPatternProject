aa920b8d67c0142de6a502d05761fa02f6bc0c54
Admin <rajeshdhonde97@gmail.com>
2022-12-10 11:49:47 +0530

    Success..

--- CustomException/src/com/velo/Tset.java
+++ CustomException/src/com/velo/Tset.java
@@ -4,9 +4,10 @@ public class Tset {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hellow");
-		System.out.println("sadads");
-		System.out.println("sadsadaddsddfgfg");
-		System.out.println("NAKLLDSLD");
+		System.out.println("Rajesh");
+		System.out.println("Pune");
+		System.out.println("7350148986");
+		System.out.println("BCA");
+		System.out.println("Pune University");
 	}
 }
